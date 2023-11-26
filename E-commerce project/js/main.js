c55927974a897675687702448751e8de79086fd4
Azad Khan <145994349+azadwebdevelop@users.noreply.github.com>
2023-11-26 04:17:20 -0800

    Add files via upload

---
+++ E-commerce project/js/main.js
@@ -0,0 +1,24 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    items:4,
+    margin:10,
+    nav:true,
+    navText:['<i class="bi bi-chevron-left"></i>','<i class="bi bi-chevron-right"></i>'],
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        576:{
+            items:2
+        },
+        768:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
